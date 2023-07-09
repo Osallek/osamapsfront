505aec9d22435301d3fdec169f225ebac2c367d4
GaetanRAYNAUD <gaetanraynaud@outlook.com>
2023-07-09 21:20:49 +0200

    jenks

--- src/utils/layer.utils.ts
+++ src/utils/layer.utils.ts
@@ -1,14 +1,19 @@
-import { quantile } from 'simple-statistics';
+import { jenks } from 'simple-statistics';
 import { DataNode } from 'types/api.types';
 import { getGradient } from 'utils/colors.utils';
 
+function onlyUnique(value: any, index: number, array: any[]) {
+  return array.indexOf(value) === index;
+}
+
 export function getAreaExpression(data: Record<string, DataNode>): Array<any> {
   const clean = Object.values(data).map(value => value.area).filter(value => value !== undefined);
-  const regionGradient = getGradient(Math.min(5, Object.keys(clean).length));
+  const regionGradient = getGradient(Math.min(7, Object.keys(clean).length));
+  const j = jenks(clean.filter(onlyUnique), regionGradient.length + 1);
 
   const colors: Array<{ value: number, color: string }> = [];
   regionGradient.forEach((color, index) => {
-    colors.push({ value: quantile(clean, (index + 1) / regionGradient.length), color });
+    colors.push({ value: j[index + 1], color });
   });
 
   const expression: Array<any> = ['case'];
