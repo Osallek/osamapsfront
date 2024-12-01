440c11b605221a49d3a81943f519311ae0548a00
GaetanRAYNAUD <gaetanraynaud@outlook.com>
2024-12-01 11:18:42 +0100

    Update ign image server

--- src/screens/map/map-style.ts
+++ src/screens/map/map-style.ts
@@ -15,7 +15,7 @@ export const mapStyle: StyleSpecification = {
     opendata: {
       type: 'raster',
       tiles: [
-        'https://wxs.ign.fr/essentiels/geoportail/wmts?layer=ORTHOIMAGERY.ORTHOPHOTOS&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}'
+        'https://data.geopf.fr/wmts?layer=ORTHOIMAGERY.ORTHOPHOTOS&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}'
       ],
       tileSize: 256,
       attribution: 'Images aériennes @ IGN',
